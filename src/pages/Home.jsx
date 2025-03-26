12e8d277fbce74b88950e82503417d0346a1fb5a
fhuang123 <fhuang2004@gmail.com>
2025-03-26 00:24:48 -0600

    fixed image paths for github pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import WatchCard from "../components/WatchCard";
 
 const watches = [
-  { name: "Seiko 5 Sports", price: "$280", image: "/seikoSport5.jpg" },
-  { name: "Seiko 5 Sports", price: "$280", image: "/seikoSport5.1.jpg" },
-  { name: "Timex waterbury", price: "$220", image: "/timexWaterbury.jpg" },
-  { name: "Orient Neo", price: "$220", image: "/orientNeo.jpg" },
-  { name: "Orient Mako", price: "$220", image: "/orientMako.jpg" },
-  { name: "Tissot Seastar", price: "$220", image: "/tissotSeastar.jpg" },
+  { name: "Seiko 5 Sports", price: "$280", image: "/ArsTemporis/seikoSport5.jpg" },
+  { name: "Seiko 5 Sports", price: "$280", image: "/ArsTemporis/seikoSport5.1.jpg" },
+  { name: "Timex waterbury", price: "$220", image: "/ArsTemporis/timexWaterbury.jpg" },
+  { name: "Orient Neo", price: "$220", image: "/ArsTemporis/orientNeo.jpg" },
+  { name: "Orient Mako", price: "$220", image: "/ArsTemporis/orientMako.jpg" },
+  { name: "Tissot Seastar", price: "$220", image: "/ArsTemporis/tissotSeastar.jpg" },
 ];
 
 export default function Home() {
