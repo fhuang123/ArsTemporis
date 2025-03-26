7af218b493f64dcbe9f5bfeae2451fa7faf76777
fhuang123 <fhuang2004@gmail.com>
2025-03-26 13:14:53 -0600

    name fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { FaInstagram } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2024 Tempus Regalis. All Rights Reserved.</p>
+      <p>&copy; 2024 Ars Temporis. All Rights Reserved.</p>
       <a href="https://instagram.com/yourwatchstore" className="instagram-link">
         <FaInstagram /> Follow us on Instagram
       </a>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <h1 className="brand-name">Tempus Regalis</h1>
+      <h1 className="brand-name">Ars Temporis</h1>
       <div className="nav-links">
-        <Link to="/">Home</Link>
+        <Link to="/arstemporis">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/preorder">Pre-Order</Link>
       </div>
